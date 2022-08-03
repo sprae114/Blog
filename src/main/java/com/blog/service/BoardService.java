c5ea008f76920aa56a123338d08aa9e3952176a6
dygks <dygks34@gmail.com>
2022-08-03 15:45:18 +0900

    [add] service의 예외를 설정해주기 위해 추가

--- src/main/java/com/blog/service/BoardService.java
+++ src/main/java/com/blog/service/BoardService.java
@@ -2,11 +2,15 @@ package com.blog.service;
 
 import com.blog.domain.Board;
 import com.blog.dto.BoardDto;
+import com.blog.dto.BoardSaveRequestDto;
+import com.blog.dto.BoardUpdateRequestDto;
+import com.blog.dto.BoardResponseDto;
 import com.blog.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +19,33 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     @Transactional
-    public Board editBoard(Long id, BoardDto boardDto){
-        Board board = boardRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
-        board.updateBoard(boardDto);
-        return board;
+    public Long save(BoardSaveRequestDto requestDto){
+        return boardRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, BoardUpdateRequestDto requestDto){
+        Board saveBoard = boardRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        saveBoard.updateBoard(requestDto);
+
+        return id;
+    }
+
+    @Transactional
+    public BoardResponseDto findById(Long id){
+        Board board = boardRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("헤당 게시글이 없습니다. id="+id));
+        return new BoardResponseDto(board);
+    }
+
+
+    @Transactional
+    public void delete (Long id){
+        Board board = boardRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        boardRepository.delete(board);
+    }
+
+
 }
