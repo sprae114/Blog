71c0b70a24c3f92734c09707dcdb33f5f2152c65
dygks <dygks34@gmail.com>
2022-08-04 21:20:57 +0900

    [add] member 검증에 따른 예외 추가

--- src/main/java/com/blog/controller/LoginController.java
+++ src/main/java/com/blog/controller/LoginController.java
@@ -1,2 +1,71 @@
-package com.blog.controller;public class LoginCotroller {
+package com.blog.controller;
+
+import com.blog.domain.Member;
+import com.blog.dto.member.MemberSaveRequestDto;
+import com.blog.repository.MemberRepository;
+import com.blog.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class LoginController {
+    private final MemberRepository memberRepository;
+    private final LoginService loginService;
+
+
+    @PostMapping ("/login")
+    public String login(@Valid @ModelAttribute("member") MemberSaveRequestDto memberSaveRequestDto,
+                        BindingResult bindingResult,
+                        @RequestParam(defaultValue = "/") String redirectURL,
+                        HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(memberSaveRequestDto.getLoginId(), memberSaveRequestDto.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        log.info("{}가 로그인 되었습니다", memberSaveRequestDto.getLoginId());
+        HttpSession session = request.getSession();
+        session.setAttribute("loginMember", loginMember);
+
+        return "redirect:" + redirectURL;
+    }
+
+
+    @PostMapping("/signup")
+    public String addMember(@ModelAttribute("member") MemberSaveRequestDto memberSaveRequestDto) {
+
+        loginService.save(memberSaveRequestDto);
+        log.info("{}가 회원가입 되었습니다", memberSaveRequestDto.getLoginId());
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        log.info("로그아웃 되었습니다");
+
+        return "redirect:/";
+    }
 }
